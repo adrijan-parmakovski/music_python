d7f67471b4cc19fb033c1661e55a7da4b39100e2
Adrijan Parmakovski <adrijan.parmakovski@gmail.com>
2021-11-22 23:01:15 +0100

    add unittest to test if all major scales are returned correctly

---
+++ tests/test_major_scales.py
@@ -0,0 +1,23 @@
+import unittest
+
+import sys, os
+sys.path.append(
+    os.path.join(
+        os.path.dirname(os.path.dirname(__file__)), 'functions'
+    )
+)
+
+from test_settings import TEST_MAJOR_SCALES
+from scales import tonic_to_scale
+
+class TestScales(unittest.TestCase):
+
+    NOTES = ['C', 'C#', 'Db', 'D', 'Eb', 'E', 'F', 'F#', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
+
+    def test_scales(self):
+        for note in self.NOTES:
+            with self.subTest(note=note):
+                self.assertEqual(tonic_to_scale(tonic=note, scale='major'), TEST_MAJOR_SCALES[note][:7])
+
+if __name__ == "__main__":
+    unittest.main()
