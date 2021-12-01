aee69c320890f1137176af787240676bc838aa99
adrijan-parmakovski <adrijan.parmakovski@gmail.com>
2021-12-01 14:37:37 +0100

    initialize package setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+    f.close()
+with open('requirements.txt', 'r', encoding='utf-8') as f:
+    requirements = f.read()
+    f.close()
+
+# setup(
+#     name = ,
+#     version = '0.1.0'
+# )
