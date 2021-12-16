926153c45a9803224bc5d4485ec100ca38bffe84
adrijan-parmakovski <adrijan.parmakovski@gmail.com>
2021-12-16 17:48:33 +0100

    add main click file

---
+++ pymusic.py
@@ -0,0 +1,5 @@
+import click
+
+@click.command()
+def cli():
+    click.echo('Welcome to PyMusiC# ! Use this tool to learn more about music theory!\nOr, like me, who can\'t figure out chords from notes.')
